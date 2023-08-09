0f97cd6f979edafa4bdfa6ad38c424ef75f2ef20
IgorFilin <filinigor@yandex.ru>
2023-08-09 23:10:11 +0300

    Добавил вывод ошибок и типизацию, логинизация

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,17 @@
 import { defineStore } from "pinia";
 import { authApi } from "@/api/auth.ts";
 import { LoginUserType } from "@/api/typesApi";
+import { useToast } from "vue-toastification";
+import axios, { AxiosError } from "axios";
+
+const toast = useToast();
 
 export const useUserStore: any = defineStore("userData", {
   state: () => {
     return {
       name: "",
       isAdmin: null,
-      messages: "",
+      messages: "" as string | undefined,
     } as UserType;
   },
   getters: {},
@@ -15,11 +19,15 @@ export const useUserStore: any = defineStore("userData", {
     async loginAction(data: LoginUserType) {
       try {
         const result = await authApi.loginUser(data);
-        console.log("result", result);
         this.messages = result.data.message;
         this.name = result.data.name;
-      } catch (error: any) {
-        console.log(error.response.data.message);
+      } catch (error) {
+        if (axios.isAxiosError(error) && error.response) {
+          const err = error as AxiosError<{ message: string }>;
+          this.messages = err.response ? err.response?.data.message : "Ошибка";
+        }
+      } finally {
+        toast(this.messages);
       }
     },
   },
