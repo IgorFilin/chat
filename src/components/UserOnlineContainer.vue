67a6c4f27ab84587c354c42127200f9f8ad738cd
IgorFilin <filinigor@yandex.ru>
2023-10-28 00:08:49 +0300

    добавил попап для личных сообщений

--- src/components/UserOnlineContainer.vue
+++ src/components/UserOnlineContainer.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="v-usersOnline" :class="{ active: isActiveUserContainer }">
+  <div
+    class="v-usersOnline"
+    :class="{ active: isActiveUserContainer }"
+    @click="showPopup = false"
+  >
     <div class="v-usersOnline__clickElem" @click="onActiveUserContainer" />
     <input class="v-usersOnline__search" type="search" v-model="searchedUser" />
     <div class="v-usersOnline__usersContainer">
       <div
         class="v-usersOnline__user"
-        :class="{ online: user.online }"
+        :class="{ online: user.online, isOpenPopup: clikedUser.id === user.id }"
+        @click="(event) => clickedUserHandler(event, user)"
         v-for="user in filteredActiveOrNotUsers"
         :key="user.id"
       >
         {{ user.name }}
+        <div
+          v-if="showPopup && clikedUser.id === user.id"
+          class="v-usersOnline__popup"
+        >
+          <div class="v-usersOnline__popupText">шёпот</div>
+        </div>
       </div>
     </div>
   </div>
@@ -24,6 +35,11 @@ const user_store = useUserStore();
 const searchedUser = ref("");
 const isActiveUserContainer = ref(false);
 const users = ref([]) as any;
+const showPopup = ref(false);
+const clikedUser = ref({
+  name: "",
+  id: "",
+});
 
 const props = defineProps({
   usersOnline: {
@@ -39,15 +55,11 @@ function onActiveUserContainer() {
   isActiveUserContainer.value = !isActiveUserContainer.value;
 }
 
-const currentUsers = computed(() => {
-  const seachValue = searchedUser.value.toLowerCase().trim();
-  if (!seachValue) {
-    return props.usersOnline;
-  }
-  return props.usersOnline?.filter((user: any) =>
-    user.name.toLowerCase().trim().includes(seachValue)
-  );
-});
+function clickedUserHandler(event: any, user: any) {
+  event.stopPropagation();
+  clikedUser.value = user;
+  showPopup.value = true;
+}
 
 onMounted(() => {
   user_store.getAllUsers();
@@ -87,8 +99,7 @@ const filteredActiveOrNotUsers = computed(() => {
 <style lang="scss">
 .v-usersOnline {
   position: absolute;
-  padding: 20px 20px 20px 50px;
-  box-sizing: border-box;
+  padding: 20px 0 20px 20px;
   width: 300px;
   height: 80%;
   max-height: 700px;
@@ -96,7 +107,7 @@ const filteredActiveOrNotUsers = computed(() => {
   border-radius: 30px 0 20px 0;
   box-shadow: 0px 0px 80px -28px rgba(0, 0, 0, 0.16);
   transition: 0.5s;
-  left: -300px;
+  left: -320px;
 
   z-index: 9;
 
@@ -124,6 +135,7 @@ const filteredActiveOrNotUsers = computed(() => {
     color: rgb(222, 212, 212);
     opacity: 0.6;
     cursor: pointer;
+    width: fit-content;
 
     &.online {
       color: rgb(25, 139, 25);
@@ -132,11 +144,13 @@ const filteredActiveOrNotUsers = computed(() => {
   }
 
   .v-usersOnline__usersContainer {
+    position: relative;
     overflow-y: scroll;
     display: flex;
     flex-direction: column;
     gap: 15px;
     height: 90%;
+    padding-top: 15px;
 
     &::-webkit-scrollbar {
       display: none;
@@ -161,6 +175,32 @@ const filteredActiveOrNotUsers = computed(() => {
     border-radius: 0 0 0 0.25em;
     transition: 0.5s;
     cursor: pointer;
+  }
+
+  .v-usersOnline__popup {
+    position: absolute;
+    background-color: #fff;
+    border-radius: 8px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+    width: fit-content;
+    height: fit-content;
+    padding: 10px 12px;
+    opacity: 1;
+    color: #555;
+    top: -8px;
+    right: -80px;
+  }
+
+  .v-usersOnline__popupText {
+    font-size: 15px;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+
+  .isOpenPopup {
+    position: relative;
   }
 }
 </style>
